ce1d91939c3ae4a6bc29ccdfe04385a3d8a45c52
jhyeon.kim <jhyeon.kim@teamsparta.co>
2024-05-30 00:55:55 +0900

    feat: /database 시작

--- babel.config.js
+++ babel.config.js
@@ -3,5 +3,16 @@ module.exports = {
   plugins: [
     ['@babel/plugin-proposal-decorators', {legacy: true}],
     ['@babel/plugin-proposal-class-properties', {loose: true}],
+    ['@babel/plugin-transform-private-methods', {loose: true}],
+    ['@babel/plugin-transform-private-property-in-object', {loose: true}],
+    [
+      'module-resolver',
+      {
+        root: ['./'],
+        alias: {
+          appjson: './app.json',
+        },
+      },
+    ],
   ],
 };
--- metro.config.js
+++ metro.config.js
@@ -1,17 +1,30 @@
-/**
- * Metro configuration for React Native
- * https://github.com/facebook/react-native
- *
- * @format
- */
+const path = require('path');
+const {getDefaultConfig} = require('metro-config');
 
-module.exports = {
-  transformer: {
-    babelTransformerPath: require.resolve(
-      'metro-react-native-babel-transformer',
-    ),
-  },
-  resolver: {
-    sourceExts: ['ts', 'tsx', 'js', 'jsx'],
-  },
-};
+module.exports = (async () => {
+  const {
+    resolver: {sourceExts, assetExts},
+  } = await getDefaultConfig();
+
+  return {
+    resolver: {
+      assetExts: [
+        ...assetExts,
+        'png',
+        'jpg',
+        'jpeg',
+        'bmp',
+        'gif',
+        'webp',
+        'svg',
+      ],
+      sourceExts: [...sourceExts, 'js', 'json', 'ts', 'tsx'],
+      extraNodeModules: {
+        appjson: path.resolve(__dirname, 'app.json'),
+        'missing-asset-registry-path': require.resolve(
+          'react-native/Libraries/Image/AssetRegistry',
+        ),
+      },
+    },
+  };
+})();
