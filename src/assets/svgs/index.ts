dcef7d4b7ba40859f0e8b2eeb2709c73351939a0
조원주 <dnjswn9178@gmail.com>
2024-06-27 15:11:56 +0900

    feat: svg 세팅 추가

---
+++ src/assets/svgs/index.ts
@@ -0,0 +1,11 @@
+// global
+export { default as HomeIcon } from './home.svg';
+export { default as SearchIcon } from './search.svg';
+export { default as SentenceIcon } from './sentence.svg';
+export { default as SettingIcon } from './setting.svg';
+export { default as CardIcon } from './cards.svg';
+export { default as CategoryIcon } from './category.svg';
+
+// category
+export { default as CloseIcon } from './close.svg';
+export { default as PlusIcon } from './plus.svg';
