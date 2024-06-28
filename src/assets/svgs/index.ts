fff1bb351394da5e0245d9f1a5416531ac1b29c7
조원주 <dnjswn9178@gmail.com>
2024-06-28 18:39:39 +0900

    feat: picture 아이콘 추가

--- src/assets/svgs/index.ts
+++ src/assets/svgs/index.ts
@@ -5,6 +5,7 @@ export { default as SentenceIcon } from './sentence.svg';
 export { default as SettingIcon } from './setting.svg';
 export { default as CardIcon } from './cards.svg';
 export { default as CategoryIcon } from './category.svg';
+export { default as PictureIcon } from './picture.svg';
 
 // category
 export { default as CloseIcon } from './close.svg';
